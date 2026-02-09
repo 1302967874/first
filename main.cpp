9443872cff9140eb674725614332bb47b5f647d2
lzp <1302967874@qq.com>
2026-02-09 13:46:44 +0800

    创建main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iosstream>
+using namespace std;
+
+void sort(int arr[], int size)
+{
+
+}
+int main()
+{
+	int arr[] = { 1,2,4,7,6,4 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	sort(arr, size);
+	return 0;
+}
